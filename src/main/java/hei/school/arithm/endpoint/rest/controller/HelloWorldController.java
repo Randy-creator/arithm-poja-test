5760ceb7cd06f875b6f8a701bb80e1c67219b28d
Randy-creator <hei.randy.2@gmail.com>
2025-06-12 04:06:25 +0300

    feat: hello world but with persistent memories

--- src/main/java/hei/school/arithm/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/hei/school/arithm/endpoint/rest/controller/HelloWorldController.java
@@ -1,12 +1,18 @@
 package hei.school.arithm.endpoint.rest.controller;
 
+
+import hei.school.arithm.service.HelloWorldService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class HelloWorldController {
+  private final HelloWorldService service;
   @GetMapping("/hello")
-  public String helloWorld() {
-    return "hello world!";
+  public String helloWorld(@RequestParam String name) {
+    return service.uploadHelloWorldMessage(name);
   }
 }
---
+++ src/main/java/hei/school/arithm/service/HelloWorldService.java
@@ -0,0 +1,34 @@
+package hei.school.arithm.service;
+
+
+import static java.io.File.createTempFile;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Duration;
+
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class HelloWorldService {
+  private final hei.school.arithm.file.bucket.BucketComponent bucketComponent;
+  @SneakyThrows
+  public String uploadHelloWorldMessage(String name) {
+    var fileSuffix = ".txt";
+    var filePrefix = "hello-world-" + name;
+    var bucketKey = filePrefix + fileSuffix;
+    var fileToUpload = createTempFile(filePrefix, fileSuffix);
+    writeMessageIntoFile("Hello World " + name + " !", fileToUpload);
+    bucketComponent.upload(fileToUpload, bucketKey);
+    return bucketComponent.presign(bucketKey, Duration.ofMinutes(5)).toString();
+  }
+  private void writeMessageIntoFile(String message, File file) throws IOException {
+    FileWriter writer = new FileWriter(file);
+    writer.write(message);
+    writer.close();
+  }
+}
