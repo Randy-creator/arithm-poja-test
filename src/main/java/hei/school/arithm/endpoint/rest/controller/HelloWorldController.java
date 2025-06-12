957dd62c87be90ec6a7ad1655003f8bb9f6aae4f
Randy-creator <hei.randy.2@gmail.com>
2025-06-12 03:35:40 +0300

    feat: hello world

---
+++ src/main/java/hei/school/arithm/endpoint/rest/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package hei.school.arithm.endpoint.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+  @GetMapping("/hello")
+  public String helloWorld() {
+    return "hello world!";
+  }
+}
